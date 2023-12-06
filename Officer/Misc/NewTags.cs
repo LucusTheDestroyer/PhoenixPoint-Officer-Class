2d6342eae682f44da0f60f320db2e85f5b80533d
Luc <lucoejames@hotmail.co.uk>
2023-12-06 09:33:36 +0000

    Close #13; Adds NewTags file containing Officer ClassTag

---
+++ Officer/Misc/NewTags.cs
@@ -0,0 +1,22 @@
+using Base.Defs;
+using PhoenixPoint.Common.Entities.GameTagsTypes;
+
+namespace Officer.Misc
+{
+    public static class NewTags
+    {
+        private static readonly DefRepository Repo = EnableMain.Repo;
+        
+        public static ClassTagDef OfficerClassTag()
+        {
+            ClassTagDef OfficerTag = (ClassTagDef)Repo.GetDef("637a5db1-ba13-4cfd-a988-c332878bb36c");
+            if (OfficerTag == null)
+            {
+                OfficerTag = Repo.CreateDef<ClassTagDef>("637a5db1-ba13-4cfd-a988-c332878bb36c");
+                OfficerTag.name = "Officer_ClassTagDef";
+                OfficerTag.ResourcePath = "Defs/GameTags/Classes/Officer_ClassTagDef";
+            }
+            return OfficerTag;
+        }
+    }
+}
