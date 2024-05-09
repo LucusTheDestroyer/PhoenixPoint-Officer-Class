6432f733ff8270d38dc7dd5d31672b88a7ea0f99
Luc <lucoejames@hotmail.co.uk>
2024-05-09 20:07:16 +0100

    Removes unnecessary logs from Mutoid recruitment patch

--- Officer/HarmonyPatches/SpecializationSelectorController_Patches.cs
+++ Officer/HarmonyPatches/SpecializationSelectorController_Patches.cs
@@ -19,18 +19,15 @@ namespace Officer.Harmony
             ResourcesUIDataDef ResUI = (ResourcesUIDataDef)Repo.GetDef("62f06399-56b4-3054-8b64-4938dd4cd329"); //"ResourcesUIDataDef"
             if(resourceImage == ResUI.GetViewForResource(ResourceType.Mutagen).Visual)
             {
-                OfficerMain.Main.Logger.LogInfo($"Mutagen resource image means this is a Mutoid");
                 for (int i=0; i < specs.Count; i++)
                 {
                     if(specs[i].ClassTag == Misc.Tags.OfficerClassTag())
                     {
-                        OfficerMain.Main.Logger.LogInfo($"Removing Officer!");
                         specs.RemoveAt(i);
                         costs.RemoveAt(i);
                     }
                 }
             }
-            
         }
     }
 }
